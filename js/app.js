f39081df6caedd482bff74e7787ef634895ad831
gerogrimaldi <gerogrimaldi2@gmail.com>
2021-04-28 00:20:39 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+// function alerta (){
+//     alert("Helo world");
+// }
+
+// document.getElementById("btn-submit").onclick = alerta();
+
+
+// function DisableCheckbox(item){
+//    item.disabled = true;
+// }
+
+// // var checkbox = document.getElementById("flexCheckDefault");
+// document.getElementById("flex").onclick = {
+//     disabled=true;
+// };
+
+// // checkbox.onclick = DisableCheckbox(checkbox);
